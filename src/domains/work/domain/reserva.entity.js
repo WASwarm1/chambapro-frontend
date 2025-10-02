896f701a501b8af60fbbf55165f1ea5771ecbd6d
Werner Lang <u202310003@upc.edu.pe>
2025-10-02 18:51:21 -0500

    feat(work): add reserva.entity

---
+++ src/domains/work/domain/reserva.entity.js
@@ -0,0 +1,19 @@
+export class Reserva {
+    /**
+     * Creates an instance of Reserva.
+     * @param id
+     * @param date
+     * @param time
+     * @param description
+     * @param client
+     * @param category
+     */
+    constructor(id, date, time, description, client, category) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.client = client;
+        this.category = category;
+    }
+}
