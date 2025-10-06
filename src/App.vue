449358ad46412e2d9cdfddd588877cf3bbcb1932
Werner Lang <u202310003@upc.edu.pe>
2025-10-06 16:18:44 -0500

    fix: issues when merged

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
 import Header from './public/components/header-content.component.vue'
 
-import workerNav from './public/components/worker-navbar.component.vue'
-
-import ReservaList from './domains/work/components/ReservaList.component.vue'
-
 // ESTO ES SOLO PARA PROBAR LO QUE ESTÁS HACIENDO ACTUALMENTE BÓRRALO LUEGO
 
 </script>
 
 <template>
+  <div>
     <Header/>
-    <workerNav/>
-    <ReservaList/>
   </div>
 </template>
 
